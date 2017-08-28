5298c7beabe9961f5d55b07a4a34be06482f4a54
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2017-08-28 03:49:54 +0000

    Delete Odd-even.c

--- Odd-even.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	int number;
-	printf("Enter Your Number :");
-	scanf("%d",&number);
-
-	number%2==0 ? printf("Enterd number is EVEN") : printf("Enterd number is ODD");
-}
