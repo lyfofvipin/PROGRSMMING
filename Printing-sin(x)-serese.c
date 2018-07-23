ee812e565c3c78c2e5c1bd9ccf03781682486597
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2018-07-23 14:28:53 +0530

    Update Printing-sin(x)-serese.c

--- Printing-sin(x)-serese.c
+++ Printing-sin(x)-serese.c
@@ -1,63 +1,63 @@
-// Printing sin(x) serese.
-// x-(x^3/3!)+(x^5/5!)-(x^7/7!)+(x^9/9!)+(x^11/11!)...
-#include<stdio.h>
-int main()
-{  
-    int n,x;
-    double result;
+//Printing sin(x) serese:-
+//x-(x^3/3!)+(x^5/5!)-(x^7/7!)+(x^9/9!)+(x^11/11!)...
 
-    double serese ( int ,int );
-    int power ( int );
-    printf("Enter value of x : ");
-    scanf("%d",&x);
+    #include<stdio.h>
+    int main()
+    {  
+        int n,x;
+        double result;
 
-    printf("Enter value of n : ");
-    scanf("%d",&n);
+        double serese ( int ,int );
 
-    result = serese(x,n);
-    printf("Resilt is  : %.3lf",result);
-    return 0;
-}
-double serese ( int x, int n )
-{
-    int i,pow,fact,number;
+        printf("Enter value of x : ");
+        scanf("%d",&x);
 
-    int power( int );
-    int factorial( int );
+        printf("Enter value of n : ");
+        scanf("%d",&n);
 
-    double res = 0.0;
-    for ( i = 1 ; i <= n ; i++ )
-    {
-        number = 2*i - 1 ; // To bring even number every time
-
-        pow = power(number);
-
-        fact = factorial(number);
-
-        if ( i%2 != 0 )
-            res = res + pow/(double)fact;
-        else
-            res = res - pow/(double)fact;
+        result = serese(x,n);
+        printf("Resilt is  : %.3lf",result);
+        return 0;
     }
-    return res;
-}
+    double serese ( int x, int n )
+    {
+        int i,pow,fact,number;
 
-int power ( int x )
-{
-    int i,pow = 1;
+        int factorial( int );
+        int power( int , int );
+        double res = 0.0;
+        for ( i = 1 ; i <= n ; i++ )
+        {
+            number = 2*i - 1 ; // To bring even number every time
 
-    for ( i = 1 ; i <= x ; i++ )
-        pow = pow*x;
-    
-    return pow;
-}
+            pow = power(x,number);
 
-int factorial( int x)
-{
-    int i,fact = 1;
+            fact = factorial(number);
 
-    for  ( i = 1 ; i <= x ; i++ )
-        fact = fact*i;
+            if ( i%2 != 0 )
+                res = res + pow/(double)fact;
+            else
+                res = res - pow/(double)fact;
+        }
+        return res;
+    }
 
-    return fact;
-}
+    int power ( int x , int n)
+    {
+        int i,pow = 1;
+
+        for ( i = 1 ; i <= n ; i++ )
+            pow = pow*x;
+
+        return pow;
+    }
+
+    int factorial( int x)
+    {
+        int i,fact = 1;
+
+        for  ( i = 1 ; i <= x ; i++ )
+            fact = fact*i;
+
+        return fact;
+    }
