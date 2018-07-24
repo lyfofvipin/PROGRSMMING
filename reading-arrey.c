b7e08368e806048e939afbd7309c34f9bea645a9
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2018-07-24 13:20:03 +0530

    Create reading-arrey.c

---
+++ reading-arrey.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  int a[5],i;
+   for (i = 0; i < 5; i++)
+     {
+       printf("enter a number \n");
+	     scanf("%d",&a[i]);
+     }
+     printf("All values are stored in the Array");
+
+}
