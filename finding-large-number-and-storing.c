33513bd3c841d52a0d55b1f3a097f7a3653e5ce4
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2018-07-24 18:51:41 +0530

    Add files via upload

---
+++ finding-large-number-and-storing.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+  int a[5],i,b[5],c[10];
+   for (i = 0; i < 5; i++)
+     {
+       printf("enter a number \n");
+	     scanf("%d",&a[i]);
+     }
+    for (i = 0; i < 5; i++)
+     {
+       printf("enter a number \n");
+	     scanf("%d",&b[i]);
+     }
+       	for (i = 0; i <5 ; i++)
+		{
+			if(a[i]>b[i])
+		          c[i]=a[i];
+			else
+				c[i]=b[i];
+		}
+	for (i = 0; i <5 ; i++)
+	  {
+	    printf("%d \t",c[i]);
+	  }
+	return 0;
+}
