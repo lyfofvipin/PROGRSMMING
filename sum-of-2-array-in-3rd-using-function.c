e144a65658af03302481ec6020282a914793f33b
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2018-07-25 22:10:58 +0530

    Update sum-of-2-array-in-3rd-using-function.c

--- sum-of-2-array-in-3rd-using-function.c
+++ sum-of-2-array-in-3rd-using-function.c
@@ -1,21 +1,25 @@
-    #include<stdio.h>
+#include<stdio.h>
     int main()
     {
         int a[] = {1,2,3,4,5};
         int b[] = {1,2,3,4,5};
+        int c[5],i;
 
     // Declaring function sum and telling it that we give 2 arrays as arguments
-        void sum ( int [] , int []);
+        void sum ( int [] , int [] , int []);
 
     // Calling sum function
         sum(a,b);
-        return 0;
+
+      for( i = 0 ; i < 5 ; i++ )
+          printf("%d  ",c[i] );
+      return 0;
     }
     // Here we have defining our function
-    void sum( int x[] , int y[] )
+    void sum( int x[] , int y[] , int z[] )
     {
         int i;
 
         for( i = 0 ; i < 5 ; i++ )
-            printf("%d  ", x[i]+y[i] );
+            z[i] = x[i]+y[i];
     }
